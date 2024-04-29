b55accf9db1c462e6ddf659aff5631ef9105b381
vadim2902 <135969069+vadim2902@users.noreply.github.com>
2024-04-29 19:00:14 +0300

    Update BreakThread.java

--- Лаба1/src/BreakThread.java
+++ Лаба1/src/BreakThread.java
@@ -6,7 +6,7 @@ public class BreakThread implements Runnable {
     @Override
     public void run() {
         try {
-            Thread.sleep(30 * 1000); // Потік спить 30 секунд перед оновленням canBreak
+            Thread.sleep(30 * 1000); 
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
