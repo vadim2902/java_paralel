f2d2bbde6ec316ce891b57c30da6b428a4efbce0
vadim2902 <135969069+vadim2902@users.noreply.github.com>
2024-04-29 18:59:31 +0300

    MainThread.java

--- Лаба1/src/MainThread.java
+++ Лаба1/src/MainThread.java
@@ -3,7 +3,7 @@ package com.company;
 public class MainThread extends Thread {
     private final int id;
     private final BreakThread breakThread;
-    private final long maxDurationMillis; // Максимальний час виконання
+    private final long maxDurationMillis; 
 
     public MainThread(int id, BreakThread breakThread, long maxDurationMillis) {
         this.id = id;
@@ -14,20 +14,20 @@ public class MainThread extends Thread {
     @Override
     public void run() {
         long sum = 0;
-        long count = 0; // Лічильник додавань
+        long count = 0; 
         long endTime = System.currentTimeMillis() + maxDurationMillis;
 
         while (System.currentTimeMillis() < endTime && !breakThread.isCanBreak()) {
             sum++;
-            count++; // Збільшуємо лічильник на кожному кроці
+            count++; 
             try {
-                Thread.sleep(1); // Маленька затримка, щоб уникнути надмірного завантаження процесора
+                Thread.sleep(1); 
             } catch (InterruptedException e) {
                 System.out.println("Thread " + id + " interrupted.");
                 break;
             }
         }
-        // Виведення інформації про потік
+       
         System.out.println("Thread ID: " + id + " - Sum: " + sum + " - Count: " + count);
     }
 }
