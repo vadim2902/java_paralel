101249835743f01164bc67f6809d2f1c5005ee5a
vadim2902 <135969069+vadim2902@users.noreply.github.com>
2024-04-29 19:01:03 +0300

    Main.java

--- Лаба1/src/Main.java
+++ Лаба1/src/Main.java
@@ -7,7 +7,9 @@ public class Main {
         new MainThread(1, breakThread, 5000).start(); 
         new MainThread(2, breakThread, 10000).start();
         new MainThread(3, breakThread, 15000).start();
-
+       // new MainThread(4, breakThread, 20000).start();
+       // new MainThread(5, breakThread, 25000).start();
+        // new MainThread(6, breakThread, 30000).start();
         new Thread(breakThread).start(); 
     }
 }
