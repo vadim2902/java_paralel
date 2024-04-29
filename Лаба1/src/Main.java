40cdd7ff4d9fbed7084d57a6d4acde74372bcf06
vadim2902 <135969069+vadim2902@users.noreply.github.com>
2024-04-29 19:00:03 +0300

    Main.java

--- Лаба1/src/Main.java
+++ Лаба1/src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         BreakThread breakThread = new BreakThread();
 
-        new MainThread(1, breakThread, 5000).start(); // Кожен потік працює 10 секунд
+        new MainThread(1, breakThread, 5000).start(); 
         new MainThread(2, breakThread, 10000).start();
         new MainThread(3, breakThread, 15000).start();
 
-        new Thread(breakThread).start(); // Управляючий потік
+        new Thread(breakThread).start(); 
     }
 }
