71f81acd2c684f2b2c2fcfae2b84d09fe276ba4c
vadim2902 <135969069+vadim2902@users.noreply.github.com>
2024-04-30 10:40:12 +0300

    Update Main.java

--- Лаба1/src/Main.java
+++ Лаба1/src/Main.java
@@ -3,13 +3,17 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         BreakThread breakThread = new BreakThread();
+        int numberOfThreads = 3; 
+        long[] durations = {5000, 10000, 15000}; 
 
-        new MainThread(1, breakThread, 5000).start(); 
-        new MainThread(2, breakThread, 10000).start();
-        new MainThread(3, breakThread, 15000).start();
-       // new MainThread(4, breakThread, 20000).start();
-       // new MainThread(5, breakThread, 25000).start();
-        // new MainThread(6, breakThread, 30000).start();
-        new Thread(breakThread).start(); 
+        if (numberOfThreads == durations.length) {
+            for (int i = 0; i < numberOfThreads; i++) {
+                new MainThread(i + 1, breakThread, durations[i]).start();
+            }
+
+            new Thread(breakThread).start(); /
+        } else {
+            System.out.println("Кількість потоків не співпадає з кількістю тривалостей виконання.");
+        }
     }
 }
